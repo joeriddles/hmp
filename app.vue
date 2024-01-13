bf1a3237fb851d2220e25ee6670cbceb21ac7e02
Joseph Riddle <joeriddles10@gmail.com>
2024-01-12 21:49:53 -0800

    Add navbar

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
-  <NuxtPage />
+  <v-app>
+    <v-app-bar elevation="1">
+      <v-btn to="/" nuxt variant="tonal">HMP</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn to="/about" nuxt>About</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
 </template>
