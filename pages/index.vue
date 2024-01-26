cdc13e74cd713f5250d25deda5489e5ffdff8829
Joseph Riddle <joeriddles10@gmail.com>
2024-01-25 22:34:12 -0800

    Fix reactivity error

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script setup lang="ts">
-const imgs = [
+const imgs = ref([
   "/IMG_4420.jpeg",
   "/IMG_4421.jpeg",
   "/IMG_4422.jpeg",
   "/IMG_4423.jpeg",
   "/IMG_4424.jpeg",
-]
+])
 </script>
 
 <style>
